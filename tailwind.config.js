949f787661448e6f594f4660fb716155bd6caef6
Falguni Shinde <141540311+FalguniShinde2004@users.noreply.github.com>
2024-07-20 13:54:02 +0530

    Task Done

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        orange: {
+          400: '#fb923c',
+          // Add other shades if necessary
+        },
+    },
+  },
+  plugins: [],
+}
+}
