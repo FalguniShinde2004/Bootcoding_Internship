75e68ffbd2d42ba890351d967acca99e6af0ce8e
Falguni Shinde <141540311+FalguniShinde2004@users.noreply.github.com>
2024-07-20 15:00:12 +0530

    Done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,8 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {
-      colors: {
-        orange: {
-          400: '#fb923c',
-          // Add other shades if necessary
-        },
-    },
+    extend: {},
   },
   plugins: [],
 }
-}
+
